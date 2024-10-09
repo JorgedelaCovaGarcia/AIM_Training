ca5f221c2dd4036dec92552864b93064b60e3eb0
hectoramadorglez <hectoramador2005@gmail.com>
2024-10-09 18:22:33 +0200

    gfhfop

--- Assets/Hector/Scripts/PlayerController.cs
+++ Assets/Hector/Scripts/PlayerController.cs
@@ -3,50 +3,61 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-
- [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    public float walkSpeed = 5f;
-    public float runSpeed = 10f;
-    public float jumpHeight = 1.9f;
-    public float gravityScale = -20f;
+    [Header("References")]
+    public Camera playerCamera;
+
+    
+
+    [Header("Movement")]
+    public float walkSpeed = 2000f;
+    
+
+    [Header("Rotation")]
+    public float rotationSensibility = 100f;
+
+   
+
+    private float cameraVerticalAngle;
     Vector3 moveInput = Vector3.zero;
+    Vector3 rotationinput = Vector3.zero;
     CharacterController characterController;
+
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        
     }
+
     private void Update()
     {
+        Look();
         Move();
-
     }
+
     private void Move()
     {
-        if (characterController.isGrounded) 
-        {
-            moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-            moveInput = Vector3.ClampMagnitude(moveInput, 1f);
-            if (Input.GetButton("Sprint"))
-            {
-                moveInput = transform.TransformDirection(moveInput) * runSpeed;
-            }
-            else 
-            {
-                moveInput = transform.TransformDirection(moveInput) * walkSpeed;
-            }
- 
-            moveInput = transform.TransformDirection(moveInput) * walkSpeed;
-            if (Input.GetButtonDown("Jump"))
-            {
-                moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
-            }
-            moveInput.y += gravityScale * Time.deltaTime;
-            characterController.Move(moveInput * Time.deltaTime);
-
-        }
+        
        
+        Vector3 movementDirection = playerCamera.transform.forward * Input.GetAxis("Vertical") + playerCamera.transform.right * Input.GetAxis("Horizontal");
+        movementDirection.y = 0;
+
+            characterController.Move(movementDirection * 5 * Time.deltaTime);
+
+
+
+    }
+
+    private void Look()
+    {
+        rotationinput.x = Input.GetAxis("Mouse X") * rotationSensibility * Time.deltaTime;
+        rotationinput.y = Input.GetAxis("Mouse Y") * rotationSensibility * Time.deltaTime;
+
+        cameraVerticalAngle = cameraVerticalAngle + rotationinput.y;
+        cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -70, 70);
+
+        transform.Rotate(Vector3.up * rotationinput.x);
+        playerCamera.transform.localRotation = Quaternion.Euler(-cameraVerticalAngle, 0f, 0f);
     }
 }
