62450a56f2f4e6fccffc35d63bbb49678d62ba3a
DESKTOP-GV16N45\Alumno <hectoramador2005@gmail.com>
2025-02-06 18:58:21 +0100

    gdhhgx

---
+++ Assets/Hector/Scripts/minijuego3/Enemigolinearecta.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigolinearecta : MonoBehaviour
+{
+    
+    public float speed = 2.0f;  
+    public float zigzagStrength = 2f;  
+    private Vector3 direction;  
+    private float zigzagDirection;  
+
+    void Start()
+    {
+        
+        direction = new Vector3(-1, 0, 1);  
+
+        
+        zigzagDirection = Random.Range(-1f, 1f);  
+    }
+
+    void Update()
+    {
+        
+        float horizontalMovement = Mathf.Sin(Time.time * speed) * zigzagStrength * zigzagDirection; 
+
+        
+        transform.Translate(new Vector3(horizontalMovement, 0, 1) * speed * Time.deltaTime);
+    }
+}
